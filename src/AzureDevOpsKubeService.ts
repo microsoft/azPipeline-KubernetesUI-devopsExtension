d03a5d17866304b62989e9f4bf31d6c53e421cf8
Sandeep Venkata <savenkat@microsoft.com>
2019-02-06 17:41:44 +0530

    Dynamically fetching kube objects (daemon/statefulSets, Services, Pods)

--- src/AzureDevOpsKubeService.ts
+++ src/AzureDevOpsKubeService.ts
@@ -1,37 +1,51 @@
 import { KubeServiceBase, KubeResourceType } from "@azurepipelines/azdevops-kube-summary/dist/Contracts/KubeServiceBase";
 
 export interface IKubeSummaryData {
-    pods: string;
-    deployments: string;
-    services: string;
-    replicaSets: string;
-    daemonSets: string;
-    statefulSets: string;
+    pods?: string;
+    deployments?: string;
+    services?: string;
+    replicaSets?: string;
+    daemonSets?: string;
+    statefulSets?: string;
 }
 
 export class AzureDevOpsKubeService extends KubeServiceBase {
-    constructor(private _kubeSummaryData: IKubeSummaryData) {
+    constructor(private _kubeSummaryData: IKubeSummaryData, private _fetchKubernetesObjects: (o: string) => Promise<IKubeSummaryData>) {
         super();
+
+        const kubeObjectsToFetch: string[] = ["daemonSets", "statefulSets", "pods", "services"];
+        const queryString: string = kubeObjectsToFetch.join(',');
+        this._dataPromise = this._fetchKubernetesObjects(queryString);
     }
 
     fetch(resourceType: KubeResourceType): Promise<any> {
         switch (resourceType) {
-            case KubeResourceType.Pods:
-                return Promise.resolve(safeParseJson(this._kubeSummaryData.pods));
             case KubeResourceType.Deployments:
                 return Promise.resolve(safeParseJson(this._kubeSummaryData.deployments));
-            case KubeResourceType.Services:
-                return Promise.resolve(safeParseJson(this._kubeSummaryData.services));
             case KubeResourceType.ReplicaSets:
                 return Promise.resolve(safeParseJson(this._kubeSummaryData.replicaSets));
             case KubeResourceType.DaemonSets:
-                return Promise.resolve(safeParseJson(this._kubeSummaryData.daemonSets));
+                return this._dataPromise.then((queryResult) => {
+                    return safeParseJson(queryResult.daemonSets);
+                });
             case KubeResourceType.StatefulSets:
-                return Promise.resolve(safeParseJson(this._kubeSummaryData.statefulSets));
+                return this._dataPromise.then((queryResult) => {
+                    return safeParseJson(queryResult.statefulSets);
+                });
+            case KubeResourceType.Pods:
+                return this._dataPromise.then((queryResult) => {
+                    return safeParseJson(queryResult.pods);
+                });
+            case KubeResourceType.Services:
+                return this._dataPromise.then((queryResult) => {
+                    return safeParseJson(queryResult.services);
+                });
         }
 
         return Promise.resolve({});
     }
+
+    private _dataPromise: Promise<IKubeSummaryData>;
 }
 
 function safeParseJson(input?: string): any {
